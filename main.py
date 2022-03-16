49d678864c86703347183837b546f2bbdd7ceef5
Harry Winer <winer.harry@gmail.com>
2022-03-16 13:04:42 +0000

    added error fallback

--- main.py
+++ main.py
@@ -5,49 +5,33 @@ from data import readJSON, readPlaylists, getCursor, setCursor
 from schedule import calculateCosts, playlistCost, trackCost
 
 def main():
-    data = readJSON("data/playlists.json")
     youtube = getAuth()
+    
     if not youtube:
         raise "Authentication Failed!"
-    
-    playlistId = createPlaylist(data["name"], youtube, data["description"] )
-    
-    for song in data["items"]:
-        songId = searchSong(song["track"]["trackName"], song["track"]["artistName"], youtube)
-        insertToPlaylist(songId, playlistId, youtube)
-    
-
-if __name__ == "__main__":
-    # youtube = getAuth()
-    
-    # if not youtube:
-    #     raise "Authentication Failed!"
     playlists = readPlaylists("data/playlists.json")
-
-    done = False
-    count = 0
-    while (not done):
-        print(f"Iteration: {count}")
-        quota_remaining = 10000
-        broken = False
-        playlistNo, trackNo = getCursor()
+    quota_remaining = 10000
+    broken = False
+    playlistNo, trackNo = getCursor()
+    try:
         for pIndex in list(range(playlistNo, len(playlists))): 
+            playlistNo = pIndex
             playlist = playlists[pIndex]
             if playlist.cost <= quota_remaining and not broken:
                 print(f"Playlist {pIndex} {playlist.name} remaining: {quota_remaining} \n Starting at: {trackNo}")
                 
-                # playlistId = createPlaylist(playlist.name, playlist.description, youtube)
+                playlistId = createPlaylist(playlist.name, playlist.description, youtube)
                 quota_remaining -= playlist.cost
                 for tIndex, track in enumerate(playlist.items[trackNo:]):
+                    trackNo += 1
                     if track.cost <= quota_remaining:
-                        # resourceId = searchSong(track.trackName, track.artistName, youtube)
-                        # insertToPlaylist(resourceId, playlistId, youtube)
+                        resourceId = searchSong(track.trackName, track.artistName, youtube)
+                        insertToPlaylist(resourceId, playlistId, youtube)
                         quota_remaining -= track.cost
-                        print(f"Track[{tIndex + trackNo}]: {track.trackName}")
+                        print(f"Track[{trackNo}]: {track.trackName}")
                     else: 
                         broken = True
                         setCursor(pIndex, tIndex)
-                        count += 1
                         break
                         
                 trackNo = 0
@@ -56,9 +40,12 @@ if __name__ == "__main__":
                 setCursor(pIndex, 0)
                 print(f"ran out with {quota_remaining} left")
                 broken = True
-                count += 1
                 break
         if not broken:
             setCursor(int(len(playlists)), 0)
-            done = True
-    print(f"Simulation done after: {count} iterations")
+    except:
+        print(f"Exception occurred on {playlistNo}, {trackNo}")
+        setCursor(playlistNo, trackNo)
+
+if __name__ == "__main__":
+    main()
